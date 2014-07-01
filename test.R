d11b90e7fbe032324a5db81e0b58cb9f8b5c23f1
justasvln <justaskalpokas@gmail.com>
2014-07-01 12:03:46 +0300

    first commit

--- test.R
+++ test.R
@@ -1,13 +1,5 @@
-#loading function train_test_vw()
-source("trainVW.R")
-
-#removing unecerasy columns 
 peeled_train<-train[,c('quantity', 'chain', 'market', 'offerdate', 'category', 'company', 'brand', 'n_buyers', 'n_buys', 'e_q', 'price', 'offervalue'):=NULL]
 peeled_test<-test[,c('quantity', 'chain', 'market', 'offerdate', 'category', 'company', 'brand', 'n_buyers', 'n_buys', 'e_q', 'price', 'offervalue'):=NULL]
-
-#calling train and test function 
-#train_test_vw(peeled_train, peeled_test)
-
 
 #all offers that are in training set
 unique_offers<-unique(peeled_train[,offer])
@@ -26,33 +18,24 @@ for (i in unique_offers){offer_prob[ unique_offers==i,probability:=x[offer == i 
 setkey(offer_prob, all_id, probability)
 
 
+#all offers that are in testing set
+test_unique_offers<-unique(peeled_test[,offer])
 
 
 
-
-#TRAINING TABLE first 10 offers
-#taking a subset of train table where the offers are the first 10 offers from unique_offers set
-
-
-
-for (i in 1:10){
-	a<-c(1:5)
-	b<-sample(a,1)
-	c<-c(1:20)
-	train_offers<-sample(c,b)
-	train_model<-peeled_train[offer %in% offer_prob[train_offers, unique_offers]] 
-
-
-	#TESTING TABLE 21-24 offers 
-	test_model<-peeled_train[offer %in% offer_prob[21:24, unique_offers]] 
-    AUC<-train_test_vw(train_model, test_model)
-
-	print(train_offers)
-	print(AUC$A)
-	test_model<-peeled_train[offer %in% offer_prob[21:24, unique_offers]] 
-	AUC<-train_test_vw(train_model, test_model)
-	print(AUC$A)
-	print(i)
+#writing down probabilities that repeater = 1 for each offer 
+x<-peeled_test[,list(offer, repeater)]
+x[,index:=1]
+test_offer_prob<-data.table(test_unique_offers)
+for (i in test_unique_offers){test_offer_prob[ test_unique_offers==i,probability:=x[offer == i ,sum(index[repeater==1]) ]/x[offer == i ,sum(index) ]]
+	test_offer_prob[ test_unique_offers==i,all_id:=x[offer == i ,sum(index) ]]
+	test_offer_prob[ test_unique_offers==i,proportion:=x[offer == i ,sum(index) ]/x[,sum(index) ]]
+	test_offer_prob[ test_unique_offers==i,proportion_pos:=x[offer == i ,sum(index[repeater==1]) ]/x[repeater==1,sum(index) ]]
 	}
-	
+setkey(test_offer_prob, all_id, probability)
+
+
+train_model<-peeled_train[offer %in% offer_prob[21:24, unique_offers]] 
+test_model<-peeled_test[offer %in% test_offer_prob[26:29, test_unique_offers]] 
+train_test_vw(train_model, test_model)
 
